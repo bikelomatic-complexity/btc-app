554efc755c9ca2c14a2d81372dc42771ef2abcce
Steven Kroh <sk.kroh@gmail.com>
2015-10-18 14:30:24 -0400

    can display a mocked list of services

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,30 @@ import ReactDOM from 'react-dom';
 import Service from './components/service';
 
 class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        services: [ {
+          name: "Joe's Pizzeria",
+          type: "Restaurant",
+          lat: "1",
+          lon: "1"
+        }, {
+          name: "Joe's Whisky Bar",
+          type: "Bar",
+          lat: "1",
+          lon: "2"
+        } ]
+      }
+  }
   render() {
+    let services = this.state.services.map((service) => {
+        return (
+          <Service json={ service }/>
+        );
+    });
     return (
-      <Service />
+      <div className="tbl">{ services }</div>
     );
   }
 }
--- www/js/components/service.js
+++ www/js/components/service.js
@@ -2,9 +2,15 @@
 import React, { Component } from 'react';
 
 export default class Service extends Component {
-	render() {
-		return (
-			<p>I hate cordova!</p>
-		);
-	}
+  render() {
+    let json = this.props.json;
+    return (
+      <div className="row">
+        <span>{ json.name }</span>
+        <span>{ json.type }</span>
+        <span>{ json.lat }</span>
+        <span>{ json.lon }</span>
+      </div>
+    );
+  }
 }
