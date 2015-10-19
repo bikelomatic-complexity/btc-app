21e00fcc7b3678043551b3fac0998514f3ea277d
Steven Kroh <sk.kroh@gmail.com>
2015-10-19 12:25:54 -0400

    Added code formatter

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Service from './components/service';
-import MyMap from './components/map'
+import MyMap from './components/map';
 
 import rest from 'rest';
 import mime from 'rest/interceptor/mime';
@@ -19,36 +19,37 @@ const server = '52.3.244.0';
 class App extends React.Component {
   /**
    * initialize state with mock service data.
+   *
    * @param {string} props - react props
    */
   constructor(props) {
-      super(props);
-      this.state = {
-        services: [ {
-          _id: "0",
-          name: "Rochester Institute of Technology",
-          type: "School",
+    super(props);
+    this.state = {
+        services: [{
+          _id: '0',
+          name: 'Rochester Institute of Technology',
+          type: 'School',
           lat: 43.0848,
           lon: -77.6744
-        } ]
+        }]
       };
   }
   componentDidMount() {
     const client = rest.wrap(mime);
-    client({ path: 'http://' + server + '/services' }).then(response => {
-      switch(response.status.code) {
+    client({path: 'http://' + server + '/services'}).then(response => {
+      switch (response.status.code) {
         case 200:
           this.setState({services: response.entity});
           break;
         default:
-          console.error("Could not load services");
+          console.error('Could not load services');
       }
-    })
+    });
   }
   render() {
     return (
       <MyMap services={this.state.services} />
-    )
+    );
   }
 }
 
--- www/js/components/service.js
+++ www/js/components/service.js
@@ -5,7 +5,7 @@ export default class Service extends Component {
   render() {
     let json = this.props.json;
     return (
-      <div className="row">
+      <div className='row'>
         <span>{ json.name }</span>
         <span>{ json.type }</span>
         <span>{ json.lat }</span>
