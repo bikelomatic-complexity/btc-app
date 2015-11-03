a8603d076fb015e177d32487e96e7865d1ab6d22
Ethan H. Jurman <ehj2229@rit.edu>
2015-11-02 23:05:22 -0500

    point card component

---
+++ www/js/components/point-card.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import { Card, CardTitle, CardActions, IconButton, CardText, CardMenu, Button } from 'react-mdl';
+
+export default class PointCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let cardStyle = {
+      width: '100%',
+      position: 'fixed',
+      bottom: '-300px',
+      transition: 'all 200ms ease',
+      'zIndex':'8'
+    }
+    let cardTitleStyle = {
+      color: '#fff',
+      height: '176px',
+      background: 'url(' + this.props.point.image + ') center / cover'
+    }
+    return (
+      <Card id="mdl-map-card" shadow={5} style={cardStyle}>
+        <CardTitle style={cardTitleStyle}>{this.props.point.name}</CardTitle>
+        <CardText>
+            {this.props.point.description}
+        </CardText>
+        <CardActions border>
+            <Button colored>See More</Button>
+        </CardActions>
+        <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+        </CardMenu>
+      </Card>
+    );
+  }
+}
