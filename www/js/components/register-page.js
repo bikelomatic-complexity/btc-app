8d2b7670a2ff09bf6f606b9d31ddea096b82ba30
Jesse Jurman <j.r.jurman@gmail.com>
2016-01-17 10:37:13 -0500

    remove extra div

--- www/js/components/register-page.js
+++ www/js/components/register-page.js
@@ -21,31 +21,29 @@ class RegisterPage extends Component {
           <Header title="Register New Account"/>
           <ACDrawer page="Login"/>
           <div className="form-column">
-            <div>
-              <div className="form-row">
-                <Textfield label="Email..."/>
-              </div>
-              <div className="form-row">
-                <Textfield label="First Name"/>
-              </div>
-              <div className="form-row">
-                <Textfield label="Last Name"/>
-              </div>
-              <div className="form-row">
-              <Textfield label="Username"/>
-              </div>
-              <div className="form-row">
-                <Textfield type="password" label="Password"/>
-              </div>
-              <div className="form-row">
-                <Textfield type="password" label="Confirm Password"/>
-              </div>
-              <div className="form-row">
-                <Button colored raised>Submit</Button>
-                <Button raised onClick={this.onCancel.bind(this)}>
-                  Cancel
-                </Button>
-              </div>
+            <div className="form-row">
+              <Textfield label="Email..."/>
+            </div>
+            <div className="form-row">
+              <Textfield label="First Name"/>
+            </div>
+            <div className="form-row">
+              <Textfield label="Last Name"/>
+            </div>
+            <div className="form-row">
+            <Textfield label="Username"/>
+            </div>
+            <div className="form-row">
+              <Textfield type="password" label="Password"/>
+            </div>
+            <div className="form-row">
+              <Textfield type="password" label="Confirm Password"/>
+            </div>
+            <div className="form-row">
+              <Button colored raised>Submit</Button>
+              <Button raised onClick={this.onCancel.bind(this)}>
+                Cancel
+              </Button>
             </div>
           </div>
         </Layout>
