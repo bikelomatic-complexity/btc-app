34f5fbda62e5c2f6d7f163881cbc5286abc95a52
Jesse Jurman <j.r.jurman@gmail.com>
2016-01-17 14:58:17 -0500

    removed dependency

--- www/js/components/settings-page.js
+++ www/js/components/settings-page.js
@@ -1,4 +1,4 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 import { Layout, Header, CardText, Button } from 'react-mdl';
 
 import ACDrawer from './ac-drawer';
