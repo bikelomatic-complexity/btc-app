424ab1eac0a4b4f884666b42c4ac345379e9464d
Steven Kroh <sk.kroh@gmail.com>
2016-01-19 15:42:18 -0500

    Added CRUD actions and action creators

--- www/js/actions/point-actions.js
+++ www/js/actions/point-actions.js
@@ -7,10 +7,36 @@ export function addPoint(point) {
   };
 }
 
+export const DELETE_POINT = 'DELETE_POINT';
+export function deletePoint(id) {
+  return {
+    type: DELETE_POINT,
+    id
+  };
+}
+
+export const UPDATE_POINT = 'UPDATE_POINT';
+export function updatePoint(id, point) {
+  return {
+    type: UPDATE_POINT,
+    id,
+    point
+  };
+}
+
+export const REPLICATE_POINT = 'REPLICATE_POINT';
+export function replicatePoint(id, point) {
+  return {
+    type: REPLICATE_POINT,
+    id,
+    point
+  };
+}
+
 export const RESET_POINTS = 'RESET_POINTS';
 export function resetPoints(points) {
   return {
     type: RESET_POINTS,
     points
   };
-};
+}
--- www/js/reducers/points.js
+++ www/js/reducers/points.js
@@ -1,5 +1,5 @@
 
-import {ADD_POINT, RESET_POINTS} from '../actions/point-actions'
+import {ADD_POINT, EDIT_POINT, DELETE_POINT} from '../actions/point-actions'
 
 export function points(state = [], action) {
   switch(action.type) {
@@ -8,8 +8,15 @@ export function points(state = [], action) {
         ...state,
         Object.assign({}, action.point)
       ];
-    case RESET_POINTS:
-      return action.points;
+    case EDIT_POINT:
+      console.log('FIXME: `EDIT_POINT`');
+      return state;
+    case DELETE_POINT:
+      console.log('FIXME: `DELETE_POINT`');
+      return state;
+    case REPLICATE_POINT:
+      console.log('FIXME: `REPLICATE_POINT`');
+      return state;
     default:
       return state;
   }
