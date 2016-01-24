334609f0355ea4c684c14b3714cd71bbc87b2a71
Steven Kroh <sk.kroh@gmail.com>
2016-01-24 12:27:03 -0500

    Cleaned up new settings duck

--- www/js/reducers/settings.js
+++ www/js/reducers/settings.js
@@ -1,17 +1,13 @@
 
 import { fromJS } from 'immutable'
+
 const ONLINE_MODE = 'pannier/settings/ONLINE_MODE';
 
-/**
- * TODO: Investigate performance hit of loading waypoints JSON into
- * the Immutable data structure
- */
 const initState = fromJS({
   onlineMode: true
 });
 
 export default function reducer(state = initState, action) {
-  // console.log('reducer' + state);
   switch(action.type) {
     case ONLINE_MODE:
       return state.set('onlineMode', action.onlineMode);
@@ -20,6 +16,9 @@ export default function reducer(state = initState, action) {
   }
 }
 
+/*
+ * Go into either online or offline mode
+ */
 export function setOnlineMode(onlineMode) {
   return { type: ONLINE_MODE, onlineMode };
 }
