b7bf958238b74a3e3581ae5ea071d3709a3688f1
Jesse Jurman <j.r.jurman@gmail.com>
2016-02-01 16:19:44 -0500

    work on location page (still needs help)

--- www/js/components/add-point-location.js
+++ www/js/components/add-point-location.js
@@ -1,14 +1,66 @@
 import React, {Component} from 'react';
 
+import PointMap from './point-map';
+import AddPointCard from './add-point-card';
+
+import { setPointLocation } from '../actions/new-point-actions';
+import { Button } from 'react-mdl';
+
+// import redux components
+import { connect } from 'react-redux';
+
 export class AddPointLocation extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
+  updateLocation(leaflet){
+    const { dispatch } = this.props;
+    const { lat, lng } = leaflet.target.getCenter();
+    dispatch(setPointLocation([lat, lng]))
+  }
+
+  onLocationSelect() {
+    // switch the page
+  }
+
   render() {
+    const { location } = this.props.newPoint;
+    const { services, alerts } = this.props;
+
+    const styleShadow = {marginLeft: '4px', marginTop: '18px', width: '25px', height: '41px'}
+    const styleMarker = {marginLeft: '0px', marginTop: '18px', height: '41px'}
+
+    let text = 'Add Point Here';
+    if (location.length !== 0) {
+      text = 'Set New Location'
+    }
     return (
       <div>
-        Add Point Location
+        <PointMap services={services}
+                  alerts={alerts}
+                  afterMoved={this.updateLocation.bind(this)}/>
+        <div className="adding-point" style={{position:'fixed', top:'50%', right:'calc(50% - 12.5px)'}}>
+          <img src="img/icons/marker-shadow.png" className="leaflet-marker-shadow" style={styleShadow}/>
+          <img src="img/icons/marker-icon.png" className="marker" style={styleMarker}/>
+        </div>
+        <Button colored onClick={this.onLocationSelect.bind(this)}>
+          { text }
+        </Button>
       </div>
     )
   }
 }
 
-export default AddPointLocation;
+function select(state) {
+  return {
+    newPoint: state.newPoint,
+    marker: state.marker,
+    services: state.points,
+    alerts: [],
+    mapState: state.mapState
+  };
+}
+
+export default connect(select)(AddPointLocation);
--- www/js/components/add-point-page.js
+++ www/js/components/add-point-page.js
@@ -5,33 +5,13 @@ import { divIcon } from 'leaflet';
 
 import { Layout, Header, Content, Button } from 'react-mdl';
 import ACDrawer from './ac-drawer';
-import PointMap from './point-map';
-import AddPointCard from './add-point-card';
 
 // import redux components
 import { connect } from 'react-redux';
 
-// import leaflet components
-import { Marker, Map, TileLayer } from 'react-leaflet';
-
-const styleShadow = {marginLeft: '4px', marginTop: '18px', width: '25px', height: '41px'}
-const styleMarker = {marginLeft: '0px', marginTop: '18px', height: '41px'}
-
 export class AddPointPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location:[0,0]
-    }
-  }
-
-  updateLocation(leaflet){
-    const { lat, lng } = leaflet.target.getCenter();
-    this.setState({location:[lat, lng]});
-  }
 
   render() {
-    const { marker, services, alerts, mapState } = this.props;
 
     return (
       <Layout fixedHeader>
@@ -67,16 +47,6 @@ export class AddPointPage extends Component {
         <div>
           {this.props.children}
         </div>
-
-        {/*
-        <PointMap services={services}
-                  alerts={alerts}
-                  afterMoved={this.updateLocation.bind(this)}/>
-        <AddPointCard history={this.props.history} location={this.state.location}/>
-        <div className="adding-point" style={{position:'fixed', top:'50%', right:'calc(50% - 12.5px)'}}>
-          <img src="img/icons/marker-shadow.png" className="leaflet-marker-shadow" style={styleShadow}/>
-          <img src="img/icons/marker-icon.png" className="marker" style={styleMarker}/>
-        </div> */}
       </Layout>
     );
   }
