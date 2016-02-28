a675dba5e293204e88ccf81a9d4c9d1f9de3aaab
Steven Kroh <sk.kroh@gmail.com>
2016-02-28 13:09:34 -0500

    upgrade browserify, babel, and babelify to fix karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,21 @@
-//karma.conf.js
-
-// This is the file that karma will parse when the service is started
-module.exports = function(config) {
-  config.set({
-    autoWatch: false,
-	browserNoActivityTimeout: 120000,
-	browserify:{
-	  transform:[ 
-	    'babelify'
-	  ]
-	},
-	browsers: ['PhantomJS'],
-	captureTimeout: 30000,
-	files: [
-	  { 
-	    pattern: './www/js/**/*.js', 
-		watched: false
-	  },
-	  { 
-	    pattern: './test/**/*.js', 
-	    watched: false
-	  }
-	],
-	frameworks: ['browserify', 'mocha', 'es6-shim'],
-	port: 8001,
-	preprocessors:{
-	  './test/**/*.js': ['browserify'],
-	  './www/js/**/*.js': ['browserify']
-	},
-	// TODO: add the coverage reporter in
-	//reporters:
-	retryLimit: 1,
-	singleRun: true
-  });
+/*global module*/
+module.exports = function( config ) {
+  config.set( {
+    frameworks: [ 'browserify', 'mocha', 'es6-shim' ],
+    port: 8001,
+    browserify: {
+      transform: [
+        'babelify'
+      ]
+    },
+    files: [
+      'test/**/*.js'
+    ],
+    preprocessors: {
+      'test/**/*.js': [ 'browserify' ]
+    },
+    browsers: [ 'PhantomJS' ],
+    retryLimit: 1,
+    singleRun: true
+  } );
 };
