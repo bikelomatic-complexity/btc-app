65d404224a57c27c09c7cf9d4edee812dc84bab9
Matt Waite <mlw6607@rit.edu>
2016-03-06 15:24:33 -0500

    Fixed linting errors

--- test/js/components/point-card-peek.spec.js
+++ test/js/components/point-card-peek.spec.js
@@ -1,13 +1,13 @@
-/*global describe before after beforeEach afterEach it $*/
-import chai, { expect } from 'chai';
+/*global beforeEach it*/
+import { expect } from 'chai';
 
 /*eslint-disable no-unused-vars*/
 import React from 'react';
 import ReactDom from 'react-dom';
 import { renderIntoDocument } from 'react-addons-test-utils';
+import { PeekPointCard } from '../../../www/js/components/peek-point-card';
 /*eslint-enable no-unused-vars*/
 
-import { PeekPointCard } from '../../../www/js/components/peek-point-card';
 import { MockPoints } from '../../mock-data/mock-points';
 
 import sd from 'skin-deep';
@@ -17,21 +17,21 @@ describe('<PointCardPeek />', function(){
   beforeEach(function(){
     this.sample = MockPoints[0];
     const tree = sd.shallowRender(
-	  <PeekPointCard
-	    point={this.sample}
-	  />
-	);
+      <PeekPointCard
+        point={this.sample}
+      />
+    );
     this.instance = tree.getMountedInstance();
     this.vdom = tree.getRenderOutput();	
   });
   
   it('should show see more', function(){
     var leftButton = this.vdom.props.children[2].props.children[0];
-    expect ( leftButton.props.label ).to.be.equal( "See More" );
+    expect ( leftButton.props.label ).to.be.equal( 'See More' );
   });
 
   it('should show close', function(){
     var rightButton = this.vdom.props.children[2].props.children[1];
-    expect ( rightButton.props.label ).to.be.equal( "Close" );
+    expect ( rightButton.props.label ).to.be.equal( 'Close' );
   });	
 });
