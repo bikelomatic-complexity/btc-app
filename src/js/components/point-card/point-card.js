a43cff6c7a6cd467e480e708dee137bac65e31b0
Jesse Jurman <j.r.jurman@gmail.com>
2016-05-18 15:06:05 -0400

    remove alpha priming

--- src/js/components/point-card/point-card.js
+++ src/js/components/point-card/point-card.js
@@ -136,17 +136,10 @@ export class PointCard extends Component {
       );
     }
 
-    if ( type === 'alert' ) {
-      // disabled menu for alert for alpha release
-      return '';
-    }
-
-    /* disabled for alpha release
     const flag = (
     <MenuItem primaryText='Flag'
       onClick={ this.navigate( '' ) } />
     );
-    */
 
     const button = (
     <IconButton>
@@ -161,7 +154,7 @@ export class PointCard extends Component {
         targetOrigin={ { horizontal: 'right', vertical: 'top' } }>
         { update }
         { rate }
-        { /*flag*/ }
+        { flag }
       </IconMenu>
       );
   }
--- src/js/containers/settings-page.js
+++ src/js/containers/settings-page.js
@@ -11,7 +11,7 @@ import Refresh from 'material-ui/lib/svg-icons/navigation/refresh';
 import Delete from 'material-ui/lib/svg-icons/action/delete';
 /*eslint-enable no-unused-vars*/
 
-//import noop from 'lodash/noop'; // not used for alpha release
+import noop from 'lodash/noop';
 
 import { resetPoints, replicatePoints } from '../reducers/points';
 import { setOnlineMode } from '../reducers/settings';
@@ -32,12 +32,12 @@ export class SettingsPage extends Component {
       subtext: 'Don\'t connect to the internet',
       toggled: !settings.onlineMode,
       onToggle: offline => setOnlineMode( !settings.onlineMode )
-    } /*, { // hidden for alpha release
+    }, {
       text: 'Download on mobile',
       subtext: 'Use 3G or 4G data',
       toggled: false,
       onToggle: noop
-    }*/ ].map( item => {
+    } ].map( item => {
       const tog = (
       <Toggle toggled={ item.toggled }
         onToggle={ item.onToggle } />
@@ -50,7 +50,6 @@ export class SettingsPage extends Component {
         );
     } );
 
-    /* hidden for alpha release
     const date = 'Last updated: ' + new Date().toLocaleDateString();
     const lastUpdated = (
     <ListItem primaryText='Update now'
@@ -58,7 +57,6 @@ export class SettingsPage extends Component {
       secondaryText={ date }
       leftIcon={ <Refresh /> } />
     );
-    */
 
     const clearPoints = (
     <ListItem primaryText='Delete cache'
@@ -88,7 +86,7 @@ export class SettingsPage extends Component {
           </List>
           <Divider />
           <List subheader='Services and Alerts'>
-            { /*lastUpdated*/ /*hidden for alpha release*/ }
+            { lastUpdated }
             { clearPoints }
           </List>
           <Divider />
