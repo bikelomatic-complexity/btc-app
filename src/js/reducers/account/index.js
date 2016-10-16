20d4ea5f50eb59fc60e754ffdde09bc3c81148b2
Austin Klein <amk5401@rit.edu>
2016-10-16 13:43:36 -0400

    new forgot password page

--- src/js/reducers/account/index.js
+++ src/js/reducers/account/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import login from './login';
 import registration from './registration';
+import forgotPassword from './forgotPassword';
 
 export { login, logout } from './login';
 export { register } from './registration';
+export { forgotPassword } from './forgotPassword';
 
-export default combineReducers( { login, registration } );
+export default combineReducers( { login, registration, forgotPassword } );
